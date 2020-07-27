91c3d7f549e0653e167fca68369daab540007f55
zaid450 <me@zaid-arshad.tech>
2020-07-27 13:32:53 -0400

    add voting

---
+++ src/commands/vote.js
@@ -0,0 +1,20 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+	"name": "vote",
+	"description": "make a vote",
+	"usage": "<description>",
+	"execute": async(message, args) => {
+		let description = args.join(" ");
+		if(!description) return message.channel.send("You need to provide a description!");
+		let voteEmbed = new MessageEmbed()
+			.setAuthor(message.author.tag, message.author.displayAvatarURL())
+			.setTitle("Vote Started")
+			.setDescription(`Please react with either of the two reactions at the bottom\n\n**Description**:\n${description}`)
+			.setFooter(message.client.user.username, message.client.user.displayAvatarURL());
+		let msg = await message.channel.send(voteEmbed);
+		await msg.react("👍");
+		await msg.react("👎");
+		message.delete();
+	}
+};

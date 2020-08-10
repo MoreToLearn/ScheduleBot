825a4c69fdad4b5d42d22f11478181a3f0e3171a
unknown <nico.03727@gmail.com>
2020-08-09 21:01:52 -0400

    test

--- src/commands/help.js
+++ src/commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
 		if (cmd) {
 			let command = message.client.commands.get(cmd);
 			if(!command) return message.channel.send("Sorry, but that command doesn't exist!");
-			return message.channel.send(new MessageEmbed().setTitle(`Info for ${cmd}`).setDescription(`Information for this command: \nName: ${command.name}\nDescription: ${command.descrpition}\nUsage: ${command.usage}`));
+			return message.channel.send(new MessageEmbed().setTitle(`Info for ${cmd}`).setDescription(`Information for this command: \nName: ${command.name}\nDescription: ${command.descrpition}\nUsage: ${command.usage}`).setFooter(message.client.user.tag, message.client.user.displayAvatarURL()));
 		}
 		return message.channel.send(new MessageEmbed().setTitle("Commands").setDescription(`List of commands: \n\`\`\`${message.client.commands.map(x => x.name).filter(x => x !== "help").join(", ")}\`\`\``));
 	}

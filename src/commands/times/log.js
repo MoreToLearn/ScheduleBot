9f0a1e955653ca31259746853a469333ee1c8be3
unknown <nico.03727@gmail.com>
2020-08-10 16:14:08 -0400

    channel => author

--- src/commands/times/log.js
+++ src/commands/times/log.js
@@ -12,32 +12,32 @@ module.exports = {
 			message.channel.send("Please check your dms from me.");
 
 		let dmChannel = await message.author.createDM();
-		await message.channel.send(new PromptEmbed("What is your first and last name?", "Please use the same name in every time log"));
+		await message.author.send(new PromptEmbed("What is your first and last name?", "Please use the same name in every time log"));
 		let name = (await message.client.prompter(dmChannel, message.author));
 		if (!name) return message.author.send(new ErrorEmbed("You ran out of time!", "You didn't provide a name in time. Please run the command again"));
 		name = name ? name.content : name;
 
-		await message.channel.send(new PromptEmbed("What is the amount of hours you spent", "PLEASE GIVE ONLY THE AMOUNT OF HOURS IN NUMBER FORM. **YOU WILL PROVIDE MINUTES LATER**. Cannot be more than 4 at a time."));
+		await message.author.send(new PromptEmbed("What is the amount of hours you spent", "PLEASE GIVE ONLY THE AMOUNT OF HOURS IN NUMBER FORM. **YOU WILL PROVIDE MINUTES LATER**. Cannot be more than 4 at a time."));
 		let hours = (await message.client.prompter(dmChannel, message.author));
 		if (!hours) return message.author.send(new ErrorEmbed("You ran out of time!", "You didn't provide an amount of hours in time. Please run the command again"));
 		hours = hours ? hours.content : hours;
 		if (parseInt(hours) > 4) return message.author.send(new ErrorEmbed("Too large!", "You have entered too many hours. Please run the command again"));
 		if (Number.isNaN(parseInt(hours))) return message.author.send(new ErrorEmbed("Incorrect!", "You didn't provide a proper amount of hours. Please run the command again"));
 
-		await message.channel.send(new PromptEmbed("What is the amount of minutes you spent", "\nExample: `25`"));
+		await message.author.send(new PromptEmbed("What is the amount of minutes you spent", "\nExample: `25`"));
 		let minutes = (await message.client.prompter(dmChannel, message.author));
 		if (!minutes) return message.author.send(new ErrorEmbed("You ran out of time!", "You didn't provide an amount of minutes in time. Please run the command again"));
 		minutes = minutes ? minutes.content : minutes;
 		if (parseInt(minutes) > 59) return message.author.send(new ErrorEmbed("Too large!", "You have entered too many minutes, please try again and correct."));
 		if (Number.isNaN(parseInt(minutes))) return message.author.send(new ErrorEmbed("Incorrect!", "You didn't provide a proper amount of minutes. Please run the command again"));
 
-		await message.channel.send(new PromptEmbed("What date was this done?", "Please format as **MM-DD-YYYY**.\n`(Ex. 4-17-2020)`"));
+		await message.author.send(new PromptEmbed("What date was this done?", "Please format as **MM-DD-YYYY**.\n`(Ex. 4-17-2020)`"));
 		let date = (await message.client.prompter(dmChannel, message.author));
 		if (!date) return message.author.send(new ErrorEmbed("You ran out of time!", "You didn't provide a date in time. Please run the command again"));
 		date = date ? date.content : date;
 		if (Number.isNaN(Date.parse(date))) return message.author.send(new ErrorEmbed("Incorrect!", "You didn't provide a valid date (Ex. 4-17-2020). Please run the command again"));
 
-		await message.channel.send(new PromptEmbed("What did you spend your time doing/teaching?", "This can be as long as you wish.\nEx. `Taught child how to tie their shoes`"));
+		await message.author.send(new PromptEmbed("What did you spend your time doing/teaching?", "This can be as long as you wish.\nEx. `Taught child how to tie their shoes`"));
 		let description = (await message.client.prompter(dmChannel, message.author));
 		if (!description) return message.author.send(new ErrorEmbed("You ran out of time!", "You didn't provide a description in time. Please run the command again"));
 		description = description ? description.content : description;

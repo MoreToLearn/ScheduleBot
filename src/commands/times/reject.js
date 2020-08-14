2d9f26a51012d8994f1217afaa3641de3f5c7241
unknown <nico.03727@gmail.com>
2020-08-14 16:06:53 -0400

    Bug fixes

--- src/commands/times/reject.js
+++ src/commands/times/reject.js
@@ -13,6 +13,6 @@ module.exports = {
         await message.client.times.delete(id);
         let submitter = await message.client.users.fetch(log.submitter);
 		if(submitter) submitter.send(new MessageEmbed().setColor("RED").setDescription(`Your time log with the id: ${log._id} with the time: ${log.time.hours} hours and ${log.time.minutes} minutes has been **REJECTED**. Please contact a Board Member if one has not contacted you already.`));
-		return message.channel.send("Time verified.");
+		return message.channel.send("Time rejected.");
 	}
 };

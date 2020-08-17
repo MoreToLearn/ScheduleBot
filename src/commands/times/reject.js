5c489bcc1f197c58ae09deb32d990a4f38811bc0
unknown <nico.03727@gmail.com>
2020-08-17 11:23:30 -0400

    add reasoning

--- src/commands/times/reject.js
+++ src/commands/times/reject.js
@@ -4,15 +4,16 @@ module.exports = {
 	"description": "reject an open time",
 	"usage": "<log-id>",
 	"example": "279",
-	"execute": async (message, [id]) => {
+	"execute": async (message, [id, ...reason]) => {
 		const log = await message.client.times.get(id);
 		if(!log) return message.channel.send("That Log does not exist.");
 		if(log.verified) return message.channel.send(`This time has already been verified by ${(await message.client.users.fetch(log.verifier)).tag}`)
 		let fetch_msg = await message.guild.channels.cache.get(message.client.data.times_id).messages.fetch(log.msg_id);
 		fetch_msg.edit(`**REJECTED** by ${message.author}`);
         await message.client.times.delete(id);
-        let submitter = await message.client.users.fetch(log.submitter);
-		if(submitter) submitter.send(new MessageEmbed().setColor("RED").setDescription(`Your time log with the id: ${log._id} with the time: ${log.time.hours} hours and ${log.time.minutes} minutes has been **REJECTED**. Please contact a Board Member if one has not contacted you already.`));
+		reason = reason.join(" ");
+		let submitter = await message.client.users.fetch(log.submitter);
+		if(submitter) submitter.send(new MessageEmbed().setColor("RED").setDescription(`Your time log with the id: ${log._id} with the time: ${log.time.hours} hours and ${log.time.minutes} minutes has been **REJECTED** because ${reason}. Please contact a Board Member if one has not contacted you already.`));
 		return message.channel.send("Time rejected.");
 	}
 };

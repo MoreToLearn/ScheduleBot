6aedc60ef8e2cb9ea08c96dc8566cb80eb49ace4
unknown <nico.03727@gmail.com>
2020-08-18 18:07:26 -0400

    update reject message

--- src/commands/times/reject.js
+++ src/commands/times/reject.js
@@ -13,7 +13,7 @@ module.exports = {
         await message.client.times.delete(id);
 		reason = reason.join(" ");
 		let submitter = await message.client.users.fetch(log.submitter);
-		if(submitter) submitter.send(new MessageEmbed().setColor("RED").setDescription(`Your time log with the id: ${log._id} with the time: ${log.time.hours} hours and ${log.time.minutes} minutes has been **REJECTED** because ${reason}. Please contact a Board Member if one has not contacted you already.`));
+		if(submitter) submitter.send(new MessageEmbed().setColor("RED").setDescription(`Your time log with the id: ${log._id} with the time: ${log.time.hours} hours and ${log.time.minutes} minutes has been **REJECTED** ${reason? `because ${  reason}`: ""}. In most cases, you can just resubmit your log again, with the proper info. If you have any questions, contact a board member`));
 		return message.channel.send("Time rejected.");
 	}
 };

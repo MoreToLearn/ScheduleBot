02cb4342df46f7fc0d923a506bebff20c289168e
zaid450 <me@zaid-arshad.tech>
2020-09-12 14:47:44 -0400

    smol update

--- src/commands/times/log.js
+++ src/commands/times/log.js
@@ -1,4 +1,6 @@
-const { MessageEmbed } = require("discord.js");
+const {
+	MessageEmbed
+} = require("discord.js");
 const ErrorEmbed = require("../../internals/models/ErrorEmbed");
 const PromptEmbed = require("../../internals/models/PromptEmbed");
 
@@ -36,13 +38,13 @@ module.exports = {
 		if (!date) return message.author.send(new ErrorEmbed("You ran out of time!", "You didn't provide a date in time. Please run the command again"));
 		date = date ? date.content : date;
 		if (Number.isNaN(Date.parse(date))) return message.author.send(new ErrorEmbed("Incorrect!", "You didn't provide a valid date (Ex. 7-25-2020). Please run the command again"));
-		if(Date.parse(date) < Date.parse("7-24-2020")) return message.author.send("Date is incorrect, can't be before 7-24-2020. Please try again.");
+		if (Date.parse(date) < Date.parse("7-24-2020")) return message.author.send("Date is incorrect, can't be before 7-24-2020. Please try again.");
 
 		await message.author.send(new PromptEmbed("What is your tutee(person being tutored)'s name?", "Please use the same name in every time log, (first name, last initial)"));
 		let tutee_name = (await message.client.prompter(dmChannel, message.author));
 		if (!tutee_name) return message.author.send(new ErrorEmbed("You ran out of time!", "You didn't provide a name in time. Please run the command again"));
 		tutee_name = tutee_name ? tutee_name.content : tutee_name;
-		
+
 		await message.author.send(new PromptEmbed("What subject (if multiple, use commas) did you teach?", "Please provide valid subjects"));
 		let subject = (await message.client.prompter(dmChannel, message.author));
 		if (!subject) return message.author.send(new ErrorEmbed("You ran out of time!", "You didn't provide a subject in time. Please run the command again"));
@@ -51,7 +53,7 @@ module.exports = {
 		await message.author.send(new PromptEmbed("Please provide the contact info that you used to contact, email or phone number only. If you don't remember this (???) then you can revisit our earlier dms and see the contact info I gave you", "Please just put the email or the phone number, example of input: `test@email.com` or `7166048061`"));
 		let contact = (await message.client.prompter(dmChannel, message.author));
 		if (!contact) return message.author.send(new ErrorEmbed("You ran out of time!", "You didn't provide contact info in time. Please run the command again"));
-		contact = contact ? contact.content : contact;	
+		contact = contact ? contact.content : contact;
 
 		await message.author.send(new PromptEmbed("What grade is your tutee in?", "Please only provide a number (ex. 6) if applies, or a word (ex. Kindergarten)"));
 		let grade = (await message.client.prompter(dmChannel, message.author));
@@ -68,8 +70,7 @@ module.exports = {
 		let timeEmbed = new MessageEmbed()
 			.setTitle("New Volunteer Time")
 			.setDescription(`**additional notes:** \`${  description}\``)
-			.addFields([
-				{
+			.addFields([{
 					"name": "Amount of Time",
 					"value": `${hours} hours and ${minutes} minutes`,
 					"inline": true
@@ -105,26 +106,26 @@ module.exports = {
 			])
 			.setFooter(`ID: ${id}. To verify, do !times verify ${id}. To reject, do !times reject ${id}`);
 		try {
-			let msg = await message.client.channels.cache.get(message.client.data.times_id).send(timeEmbed);
-			message.client.times.create(id,
-				{
-					"name": name,
-					"description": description,
-					"time": {
-						"hours": hours,
-						"minutes": minutes
-					},
-					"tutee": {
-						"name": tutee_name,
-						"contact": contact,
-						"grade": grade
-					},
-					"subject": subject,
-					"date": date,
-					"msg_id": msg.id,
-					"submitter": message.author.id
-				});
-		} catch(e) {
+			let role = message.guild.roles.cache.find(x => x.name.toLowerCase() === "time verifier");
+			let msg = await message.client.channels.cache.get(message.client.data.times_id).send(role.toString(), timeEmbed);
+			message.client.times.create(id, {
+				"name": name,
+				"description": description,
+				"time": {
+					"hours": hours,
+					"minutes": minutes
+				},
+				"tutee": {
+					"name": tutee_name,
+					"contact": contact,
+					"grade": grade
+				},
+				"subject": subject,
+				"date": date,
+				"msg_id": msg.id,
+				"submitter": message.author.id
+			});
+		} catch (e) {
 			return message.channel.send("Issue Saving time to Database. Contact Zaid.");
 		}
 		message.author.send(`Your time log has been recieved and will be reviewed before being inputted. Please screenshot the details below for your records.\n\nid: ${id}\nname: ${name}\ntime: ${hours} hours and ${minutes} minutes`);
